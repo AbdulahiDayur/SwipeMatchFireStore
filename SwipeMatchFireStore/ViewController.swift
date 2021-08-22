e03e8e953b5a34af3f15ae62013a1aede4e96ce7
Abdul Dayur <Abdul.dayur@nike.com>
2021-08-21 19:06:38 -0700

    testing stackView layout

--- SwipeMatchFireStore/ViewController.swift
+++ SwipeMatchFireStore/ViewController.swift
@@ -11,7 +11,27 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let redView = UIView()
+        redView.backgroundColor = .red
+        let blueView = UIView()
+        blueView.backgroundColor = .blue
+        
+        let stackView = UIStackView(arrangedSubviews: [redView, blueView])
+        stackView.distribution = .fillEqually
+        stackView.axis = .vertical
+        
+        view.addSubview(stackView)
+        stackView.frame = .init(x: 50, y: 90, width: 300, height: 200)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -10)
+        ])
     }
 
 
