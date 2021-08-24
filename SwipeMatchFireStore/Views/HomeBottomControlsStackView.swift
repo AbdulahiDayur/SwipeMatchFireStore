3211d3ac55054327b3454c0d701a2ee30cf26d45
Abdul Dayur <Abdul.dayur@nike.com>
2021-08-23 20:56:26 -0700

    refactoring uistackview

---
+++ SwipeMatchFireStore/Views/HomeBottomControlsStackView.swift
@@ -0,0 +1,38 @@
+//
+//  HomeBottomControlsStackView.swift
+//  SwipeMatchFireStore
+//
+//  Created by Abdul Dayur on 8/23/21.
+//
+
+import UIKit
+
+class HomeBottomControlsStackView: UIStackView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        distribution = .fillEqually
+        heightAnchor.constraint(equalToConstant: 120).isActive = true
+        
+        
+       // bottom row of buttons
+       let subViews = [UIImage(imageLiteralResourceName: "refresh_circle"), UIImage(named: "dismiss_circle"),
+        UIImage(named: "super_like_circle"),UIImage(named: "like_circle"),UIImage(named: "boost_circle")].map { (image) -> UIView in
+            let button = UIButton(type: .system)
+            button.setImage(image?.withRenderingMode(.alwaysOriginal), for: .normal)
+            return button
+        }
+        
+        for button in subViews {
+            addArrangedSubview(button)
+        }
+        
+    }
+    
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
