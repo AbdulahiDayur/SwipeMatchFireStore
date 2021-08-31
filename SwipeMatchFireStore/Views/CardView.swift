b319fc6e21a6b15764b05e2344e654f4df18d322
Abdul Dayur <Abdul.dayur@nike.com>
2021-08-30 21:06:38 -0700

    adding action function to panGesture state changes

--- SwipeMatchFireStore/Views/CardView.swift
+++ SwipeMatchFireStore/Views/CardView.swift
@@ -13,17 +13,41 @@ class CardView: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
         
-        // Custom drawing code
         layer.cornerRadius = 10
         clipsToBounds = true
         
         addSubview(imageView)
         imageView.fillSuperview()
+        
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
+        addGestureRecognizer(panGesture)
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    @objc private func handlePan(gesture: UIPanGestureRecognizer) {
+
+        switch gesture.state {
+        
+        case .changed:
+            let translation = gesture.translation(in: nil)
+            self.transform = CGAffineTransform(translationX: translation.x, y: translation.y)
+
+        case .ended:
+            UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 0.6,
+                           initialSpringVelocity: 0.1, options: .curveEaseOut,
+                           animations: {self.transform = .identity})
+
+        default:
+            ()
+        }
     }
     
 }
