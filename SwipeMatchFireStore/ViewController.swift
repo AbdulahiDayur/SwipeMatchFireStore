0c4a1f005d0221498fc120bf3184832d6a6266bb
Abdul Dayur <Abdul.dayur@nike.com>
2021-08-30 21:05:38 -0700

    bringing photo to front of subview when panGesturing

--- SwipeMatchFireStore/ViewController.swift
+++ SwipeMatchFireStore/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     let cardsDeckView = UIView()
     let bottomStackView = HomeBottomControlsStackView()
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -21,6 +22,7 @@ class ViewController: UIViewController {
     }
     
     private func setupDummyCards() {
+
         let cardView = CardView()
         cardsDeckView.addSubview(cardView)
         cardView.fillSuperview()
@@ -38,6 +40,7 @@ class ViewController: UIViewController {
         
         overallStackView.isLayoutMarginsRelativeArrangement = true
         overallStackView.layoutMargins = .init(top: 0, left: 8, bottom: 0, right: 8)
+        overallStackView.bringSubviewToFront(cardsDeckView)
     }
 
 
