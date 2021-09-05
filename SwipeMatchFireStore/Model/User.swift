608c851cc592e8924afb091b51f8544f949f5d0c
Abdul Dayur <Abdul.dayur@nike.com>
2021-09-04 21:02:32 -0700

    Defining properties for our model layer

---
+++ SwipeMatchFireStore/Model/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  SwipeMatchFireStore
+//
+//  Created by Abdul Dayur on 9/4/21.
+//
+
+import Foundation
+
+struct User {
+    // Defining our properties for our model layer
+    let name: String
+    let age: Int
+    let profession: String
+    let imageName: String
+}
+
