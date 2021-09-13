052cc8620873c2f080cb55945d4e1e980bb5dc62
Abdul Dayur <Abdul.dayur@nike.com>
2021-09-12 18:05:22 -0700

    substituted user model for cardViewModel

--- SwipeMatchFireStore/Controllers/HomeController.swift
+++ SwipeMatchFireStore/Controllers/HomeController.swift
@@ -13,9 +13,14 @@ class HomeController: UIViewController {
     let cardsDeckView = UIView()
     let buttonsStackView = HomeBottomControlsStackView()
     
-    let users = [
-        User(name: "Kelly", age: 23, profession: "Music DJ", imageName: "lady5c"),
-        User(name: "Jane", age: 18, profession: "Teacher", imageName: "lady4c")
+//    let users = [
+//        User(name: "Kelly", age: 23, profession: "Music DJ", imageName: "lady5c"),
+//        User(name: "Jane", age: 18, profession: "Teacher", imageName: "lady4c")
+//    ]
+    
+    let cardViewModel = [
+        User(name: "Kelly", age: 23, profession: "Music DJ", imageName: "lady5c").toCardViewModel(),
+        User(name: "Jane", age: 18, profession: "Teacher", imageName: "lady4c").toCardViewModel()
     ]
     
     override func viewDidLoad() {
@@ -24,18 +29,13 @@ class HomeController: UIViewController {
         setupDummyCards()
     }
     
-    fileprivate func setupDummyCards() {
-        users.forEach { (user) in
+    private func setupDummyCards() {
+        
+        cardViewModel.forEach { (cardVM) in
             let cardView = CardView(frame: .zero)
-            cardView.imageView.image = UIImage(named: user.imageName)
-            cardView.informationLabel.text = "\(user.name) \(user.age)\n\(user.profession)"
-            
-            let attributedText = NSMutableAttributedString(string: user.name, attributes: [.font: UIFont.systemFont(ofSize: 32, weight: .heavy)])
-            attributedText.append(NSAttributedString(string: "  \(user.age)", attributes: [.font: UIFont.systemFont(ofSize: 24, weight: .regular)]))
-            
-            attributedText.append(NSAttributedString(string: "\n\(user.profession)", attributes: [.font: UIFont.systemFont(ofSize: 20, weight: .regular)]))
-            
-            cardView.informationLabel.attributedText = attributedText
+            cardView.imageView.image = UIImage(named: cardVM.imageName)
+            cardView.informationLabel.attributedText = cardVM.attributedString
+            cardView.informationLabel.textAlignment = cardVM.textAlignment
             
             cardsDeckView.addSubview(cardView)
             cardView.fillSuperview()
@@ -43,13 +43,17 @@ class HomeController: UIViewController {
     }
     
     private func setupLayout() {
-        let overallStackView = UIStackView(arrangedSubviews: [topStackView, cardsDeckView, buttonsStackView])
+        let overallStackView  = UIStackView(arrangedSubviews: [topStackView, cardsDeckView, buttonsStackView])
         overallStackView.axis = .vertical
         view.addSubview(overallStackView)
-        overallStackView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.trailingAnchor)
+        
+        overallStackView.anchor(top     : view.safeAreaLayoutGuide.topAnchor,
+                                leading : view.leadingAnchor,
+                                bottom  : view.safeAreaLayoutGuide.bottomAnchor,
+                                trailing: view.trailingAnchor)
+        
         overallStackView.isLayoutMarginsRelativeArrangement = true
         overallStackView.layoutMargins = .init(top: 0, left: 12, bottom: 0, right: 12)
-        
         overallStackView.bringSubviewToFront(cardsDeckView)
     }
 
