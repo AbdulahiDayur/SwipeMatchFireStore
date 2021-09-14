c564eb0cb19444536b9a86d263d601dad7d1eab6
Abdul Dayur <Abdul.dayur@nike.com>
2021-09-13 20:26:25 -0700

    added new advertiser model for card images

---
+++ SwipeMatchFireStore/Model/Advertiser.swift
@@ -0,0 +1,24 @@
+//
+//  Advertiser.swift
+//  SwipeMatchFireStore
+//
+//  Created by Abdul Dayur on 9/13/21.
+//
+
+import UIKit
+
+struct Advertiser: ProducesCardViewModel {
+    let title: String
+    let brandName: String
+    let posterPhotoName: String
+    
+    
+    func toCardViewModel() -> CardViewModel {
+        
+        let attributedText = NSMutableAttributedString(string: title, attributes: [.font: UIFont.systemFont(ofSize: 34, weight: .heavy)])
+        attributedText.append(NSAttributedString(string: "\n" + brandName, attributes: [.font: UIFont.systemFont(ofSize: 24, weight: .bold)]))
+        
+        return CardViewModel(imageName: posterPhotoName, attributedString: attributedText, textAlignment: .center)
+    }
+    
+}
