19a5d43f37a6a24900db496f337ddb93f3b4bee6
Abdul Dayur <Abdul.dayur@nike.com>
2021-09-16 13:25:48 -0700

    changed our modelView struct to a class for ease of use

--- SwipeMatchFireStore/ViewModels/CardViewModel.swift
+++ SwipeMatchFireStore/ViewModels/CardViewModel.swift
@@ -11,14 +11,33 @@ import UIKit
 // Supports many different types of model objects (for cards)
 // We'll define the properties that are view will display/render out
 
+// View Model is supposed to represent the state of our View
+
 protocol ProducesCardViewModel{
     func toCardViewModel() -> CardViewModel
 }
 
-struct CardViewModel {
-    let imageName: String
+class CardViewModel {
+    let imageNames: [String]
     let attributedString: NSAttributedString
     let textAlignment: NSTextAlignment
+    
+    init(imageNames: [String], attributedString: NSAttributedString, textAlignment: NSTextAlignment) {
+        self.imageNames = imageNames
+        self.attributedString = attributedString
+        self.textAlignment = textAlignment
+    }
+    
+    var imageIndex = 0
+    
+    func advanceToNextPhoto() {
+        imageIndex = imageNames.count - 1
+        
+    }
+    
+    func goToPreviousPhoto() {
+        imageIndex -= 1
+    }
 }
 
 
