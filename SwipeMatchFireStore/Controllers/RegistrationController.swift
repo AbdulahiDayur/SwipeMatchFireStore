0b037750313d0a7c3854c73420b85349c75b7ebe
Abdul Dayur <Abdul.dayur@nike.com>
2021-09-18 20:13:50 -0700

    added registration vc

---
+++ SwipeMatchFireStore/Controllers/RegistrationController.swift
@@ -0,0 +1,18 @@
+//
+//  RegistrationController.swift
+//  SwipeMatchFireStore
+//
+//  Created by Abdul Dayur on 9/18/21.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+
+}
