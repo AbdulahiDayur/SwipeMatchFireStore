fb992b326403dc15e4c3b2e5aa70b74196bc9d07
Abdul Dayur <Abdul.dayur@nike.com>
2021-09-24 21:55:17 -0700

    using JGProgressHUD to display registration tap

--- SwipeMatchFireStore/Controllers/RegistrationController.swift
+++ SwipeMatchFireStore/Controllers/RegistrationController.swift
@@ -85,6 +85,8 @@ class RegistrationController: UIViewController {
         return button
     }()
     
+    let registerHud = JGProgressHUD(style: .dark)
+    
     @objc func handleRegister() {
         print("Register our User in FireBase Auth")
         self.handleTapDismiss()
@@ -92,20 +94,22 @@ class RegistrationController: UIViewController {
         guard let email = emailTextField.text else { return }
         guard let password = passwordTextField.text else { return }
         
+        registerHud.textLabel.text = "Register"
+        registerHud.show(in: view)
+        
         Auth.auth().createUser(withEmail: email, password: password) { (result, err) in
             
             if let err = err {
-                print("ERROR ALERTTTTTTT: ", err)
                 self.showHudWithError(error: err)
                 return
             }
             
             print("Successfully registered user:", result?.user.uid ?? "")
-            print("WHAT IS RESULT YOU ASK?", result ?? "")
         }
     }
     
     private func showHudWithError(error: Error) {
+        registerHud.dismiss()
         let hud = JGProgressHUD(style: .dark)
         hud.textLabel.text = "Failed registration"
         hud.detailTextLabel.text = error.localizedDescription
