1ad31e898c0c207869acf3b1e225b803a837bd90
Abdul Dayur <Abdul.dayur@nike.com>
2021-09-25 16:36:23 -0700

    setting up a registration observer

--- SwipeMatchFireStore/Controllers/RegistrationViewModel.swift
+++ SwipeMatchFireStore/Controllers/RegistrationViewModel.swift
@@ -9,24 +9,21 @@ import UIKit
 
 class RegistrationViewModel {
     
-    var image: UIImage? {
-        didSet {
-            imageObserver?(image)
-        }
-    }
-    
-    var imageObserver: ((UIImage?) -> ())?
-    
+    var image: UIImage? { didSet {imageObserver?(image)} }
+    var isRegistering : Bool? { didSet { isRegisteringObserver?(isRegistering)} }
     var fullName: String? { didSet {checkForValidity()} }
     var email: String? { didSet {checkForValidity()} }
     var password: String? { didSet {checkForValidity()} }
+    
+    
+    // Reactive Programming
+    var isFormValidObserver: ((Bool) -> ())?
+    var imageObserver: ((UIImage?) -> ())?
+    var isRegisteringObserver: ((Bool?) -> ())?
     
     private func checkForValidity() {
         let isFormVaild = fullName?.isEmpty == false && email?.isEmpty == false && password?.isEmpty == false
         
         isFormValidObserver?(isFormVaild)
     }
-    
-    // Reactive Programming
-    var isFormValidObserver: ((Bool) -> ())?
 }
