6f9ab129a1ba24ae2f64dda16159bb148347c0fc
Abdul Dayur <Abdul.Dayur@nike.com>
2021-10-14 18:15:41 -0700

    refactored HomeBottomControlsStackView

--- SwipeMatchFireStore/Views/HomeBottomControlsStackView.swift
+++ SwipeMatchFireStore/Views/HomeBottomControlsStackView.swift
@@ -9,26 +9,31 @@ import UIKit
 
 class HomeBottomControlsStackView: UIStackView {
     
+    
+    static func createButton(image: UIImage) -> UIButton {
+        let button = UIButton(type: .system)
+        button.setImage(image.withRenderingMode(.alwaysOriginal), for: .normal)
+        button.imageView?.contentMode = .scaleAspectFill // buttons might be stretched out, this helps.
+        
+        return button
+    }
+    
+    let refreshButton = createButton(image: #imageLiteral(resourceName: "refresh_circle"))
+    let dismissButton = createButton(image: #imageLiteral(resourceName: "dismiss_circle"))
+    let superLikeButton = createButton(image: #imageLiteral(resourceName: "super_like_circle"))
+    let likeButton = createButton(image: #imageLiteral(resourceName: "like_circle"))
+    let boostButton = createButton(image: #imageLiteral(resourceName: "boost_circle"))
+    
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         distribution = .fillEqually
         heightAnchor.constraint(equalToConstant: 100).isActive = true
         
-        
-       // bottom row of buttons
-       let subViews = [UIImage(imageLiteralResourceName: "refresh_circle"), UIImage(named: "dismiss_circle"),
-        UIImage(named: "super_like_circle"),UIImage(named: "like_circle"),UIImage(named: "boost_circle")].map { (image) -> UIView in
-            let button = UIButton(type: .system)
-            button.setImage(image?.withRenderingMode(.alwaysOriginal), for: .normal)
-            return button
+        [refreshButton, dismissButton, superLikeButton, likeButton, boostButton].forEach { (UIButton) in
+            self.addArrangedSubview(UIButton)
         }
-        
-        for button in subViews {
-            addArrangedSubview(button)
-        }
-        
     }
-    
     
     required init(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
